6804843bc1b61f15f4b70b91f9812ebb9cb87350
apple <@mail.ru>
2025-05-26 20:43:30 +0700

    add Item model and declare table schema with SQLAlchemy ORM

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./items.db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(Float, nullable=True)
+    price = Column(Float, nullable=False)
+    tax = Column(Float, nullable=True)
