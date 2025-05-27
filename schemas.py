6e80f69f46a781d2995b8c1de56bf1f547d09a3f
apple <@mail.ru>
2025-05-27 18:31:58 +0700

    define base schema for Item with common fields

--- schemas.py
+++ schemas.py
@@ -1,3 +1,8 @@
 from pydantic import BaseModel
 from typing import Optional
 
+class ItemBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
