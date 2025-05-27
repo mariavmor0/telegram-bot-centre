e62e36eafc415073e4862399d92dce58f9b6812d
apple <@mail.ru>
2025-05-27 20:41:55 +0700

    add test for creating a new item

--- test_main.py
+++ test_main.py
@@ -1,4 +1,17 @@
+from http.client import responses
+
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
+
+def test_create_item():
+    response = client.post('/items/', json={
+        'name': 'Test item',
+        'description': 'A simple test item',
+        'price': 9.99,
+        'tax': 1.00
+    })
+    assert response.status_code == 200
+    assert response.json()['name'] == 'Test item'
+    assert response.json()['price'] == 9.99
