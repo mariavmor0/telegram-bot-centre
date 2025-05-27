331c3c26afe2f15f181728eef0ba0cec5c9c5b6e
apple <@mail.ru>
2025-05-27 22:16:03 +0700

    correction of errors in the Class Item (Base)

--- models.py
+++ models.py
@@ -6,6 +6,6 @@ class Item(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    description = Column(Float, nullable=True)
+    description = Column(String, nullable=True)
     price = Column(Float, nullable=False)
     tax = Column(Float, nullable=True)
