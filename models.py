08a5bddc7c05c37583ba06146b5eded1a6c8abb5
apple <@mail.ru>
2025-05-27 18:03:49 +0700

    add fields to Item model with types and constraints

--- models.py
+++ models.py
@@ -3,3 +3,9 @@ from database import Base
 
 class Item(Base):
     __tablename__ = 'items'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(Float, nullable=True)
+    price = Column(Float, nullable=False)
+    tax = Column(Float, nullable=True)
