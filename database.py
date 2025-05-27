8c9e5845ba63a47c84665c2f5d4ebee1f164bb94
apple <@mail.ru>
2025-05-27 17:17:38 +0700

    configure sessionmaker for database sessions

--- database.py
+++ database.py
@@ -4,3 +4,5 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./items.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
