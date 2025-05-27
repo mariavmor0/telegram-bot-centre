24aaad72e91cd3c16578b6a5ac0d202ef0cfe149
apple <@mail.ru>
2025-05-27 17:16:42 +0700

    create SQLAlchemy engine with SQLite configuration

--- database.py
+++ database.py
@@ -2,3 +2,5 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./items.db'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
