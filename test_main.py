5c7d90b5c4167c31dec22cfef6f43d499b338232
apple <@mail.ru>
2025-05-27 21:00:12 +0700

    add test for deleting an item and verifying removal

--- test_main.py
+++ test_main.py
@@ -33,3 +33,37 @@ def test_read_items():
     get_response = client.get(f'/items/{item_id}')
     assert get_response.status_code == 200
     assert get_response.json()['id'] == item_id
+
+def test_update_item():
+    post_response = client.post('/items/', json={
+        'name': 'Old Name',
+        'description': 'Old Desc',
+        'price': 10.0,
+        'tax': 1.0
+    })
+    item_id = post_response.json()['id']
+
+    update_response = client.put(f'/items/{item_id}', json={
+        'name': 'New Name',
+        'price': 15.0
+    })
+
+    assert update_response.status_code == 200
+    assert update_response.json()['name'] == 'New Name'
+    assert update_response.json()['price'] == 15.0
+
+def test_delete_item():
+    post_responce = client.post('/items/', json={
+        'name': 'To Delete',
+        'description': 'To be deleted',
+        'price': 3.0,
+        'tax': 0.2
+    })
+    item_id = post_responce.json()['id']
+
+    delete_response = client.delete(f'/items/{item_id}')
+    assert delete_response.status_code == 200
+    assert 'has been deleted' in delete_response.json()['message']
+
+    get_response = client.get(f'/items/{item_id}')
+    assert get_response.status_code == 200
