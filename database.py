dfeeaf4de2f6aa2ccc922b70eb609bb91a612e01
apple <@mail.ru>
2025-05-29 21:54:10 +0700

    added import from library

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./items.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
