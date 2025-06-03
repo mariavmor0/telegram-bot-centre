5a0d36a6b2c8dd26be9c09da4cbe986a23b1531a
apple <@mail.ru>
2025-06-03 21:06:55 +0700

    initializing SQLite Database and Configuring SQLAlchemy

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from typing import Optional, List
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./test.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bird=engine)
+Base = declarative_base()
