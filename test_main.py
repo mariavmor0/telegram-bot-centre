2664a6e7c3fe53c31d7a571a0bf09ab86f4cee7a
apple <@mail.ru>
2025-06-04 00:16:30 +0700

    fix of errors

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ from main import app
 client = TestClient(app)
 created_item_id = None
 
-def test_root():
+def test_read_root():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {'message': 'Hello from FastAPI'}
