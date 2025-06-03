227a626c32750647855b29516c3dbe084835b503
apple <@mail.ru>
2025-06-03 23:03:56 +0700

    check for receiving element by ID

--- test_main.py
+++ test_main.py
@@ -22,3 +22,10 @@ def test_create_item():
     assert data['name'] == 'Test Item'
     assert data['price'] == 100.0
     created_item_id = data['id']
+
+def test_read_item():
+    response = client.get(f'/items/{created_item_id}')
+    assert response.status_code == 200
+    data = response.json()
+    assert data['id'] == created_item_id
+    assert data['name'] == 'Test Item'
