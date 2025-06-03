b1ed8c671e62131d0701d89929ac78fda369ad8c
apple <@mail.ru>
2025-06-03 22:06:45 +0700

    implemented creation of an element (POST /items/)

--- main.py
+++ main.py
@@ -54,3 +54,12 @@ app.get('/')
 def root():
     return {'message': 'hello from FastAPI'}
 
+@app.post('/items/', response_model=ItemOut)
+def create_item(item: ItemCreate, db: Session = Depends(get_db)):
+    db_item = Item(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
