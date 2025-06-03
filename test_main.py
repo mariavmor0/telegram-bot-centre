64a85b48c20688c1287838426e11edba6e8d60c4
apple <@mail.ru>
2025-06-03 22:56:11 +0700

    checking basic GET request for '/'

--- test_main.py
+++ test_main.py
@@ -3,3 +3,8 @@ from main import app
 
 client = TestClient(app)
 created_item_id = None
+
+def test_root():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'hello from FastAPI'}
